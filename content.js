a7d604f13ed709251685ae3e179cc57af64b2fcd
getCurrentThread <wak8835@gmail.com>
2022-06-29 14:02:17 +0900

    Feat 주소를 덜 바꾸는 방향으로 변경

--- content.js
+++ content.js
@@ -1,18 +1,32 @@
 (() =>{
     // If it is set to a detailed match on the manifest, it sometimes fails, so the exception is handled like this...
-    if(window.name !== "cafe_main") return;
+    if(window.name !== 'cafe_main') return;
 
     try{
-        // Change the browser address to the current main content url.
-        window.parent.history.replaceState(null, null, document.location.href);
+        let _done = false;
+        // If it is the main site, change the browser address the global variable Cafe address url. 'g_sCafeHome'
+        if(document.location.href.includes('cafe.naver.com/MyCafeIntro.nhn')){
+            document.documentElement.setAttribute('onreset', 'window.parent.history.replaceState(null, null, g_sCafeHome);');
+            document.documentElement.dispatchEvent(new CustomEvent('reset'));
+            document.documentElement.removeAttribute('onreset');
+            return;
+        }
 
-        // If there is a post, change it to a clearer address url.
+        // Change the browser address to the current main content url.
+        setTimeout(() =>{
+            if(!_done){
+                window.parent.history.replaceState(null, null, document.location.href);
+            }
+        }, 500);
+
+        // If it is a post, change it to a clearer address url.
         $(document).arrive('a#spiButton.naver-splugin',  {
             existing: true
         },function(spiBtn){
             const lastCafeMainUrl = spiBtn.getAttribute('data-url');
             if(lastCafeMainUrl != null){
                 window.parent.history.replaceState(null, null, lastCafeMainUrl);
+                _done = true;
             }
         });
     }
