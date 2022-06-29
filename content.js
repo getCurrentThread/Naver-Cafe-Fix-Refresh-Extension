3ffb3d3e632f07ea444ae48ff9985bb65669f5a4
getCurrentThread <wak8835@gmail.com>
2022-06-29 14:30:23 +0900

    Feat 보다 빠른 브라우저 주소 변경과 실패 예측에 따른 변경 기능

--- content.js
+++ content.js
@@ -4,31 +4,37 @@
 
     try{
         let _done = false;
-        // If it is the main site, change the browser address the global variable Cafe address url. 'g_sCafeHome'
-        if(document.location.href.includes('cafe.naver.com/MyCafeIntro.nhn')){
+        const url = document.location.href;
+        // 1. If it is the main site, change the browser address the global variable Cafe address url. 'g_sCafeHome'
+        if(url.includes('cafe.naver.com/MyCafeIntro.nhn')){
             document.documentElement.setAttribute('onreset', 'window.parent.history.replaceState(null, null, g_sCafeHome);');
             document.documentElement.dispatchEvent(new CustomEvent('reset'));
             document.documentElement.removeAttribute('onreset');
             return;
         }
 
-        // Change the browser address to the current main content url.
-        setTimeout(() =>{
-            if(!_done){
-                window.parent.history.replaceState(null, null, document.location.href);
-            }
-        }, 500);
+        // 2. Change the browser address to the current main content url.
+        if(!['ArticleRead.nhn', '/articles/'].some(x => url.includes(x))){
+            window.parent.history.replaceState(null, null, url);
+        }
 
-        // If it is a post, change it to a clearer address url.
+        // 3. If it is a post, change it to a clearer address url.
         $(document).arrive('a#spiButton.naver-splugin',  {
             existing: true
         },function(spiBtn){
-            const lastCafeMainUrl = spiBtn.getAttribute('data-url');
-            if(lastCafeMainUrl != null){
-                window.parent.history.replaceState(null, null, lastCafeMainUrl);
+            const inUrl = spiBtn.getAttribute('data-url');
+            if(inUrl != null){
+                window.parent.history.replaceState(null, null, inUrl);
                 _done = true;
             }
         });
+
+        // 4. If some time has passed, it is assumed that the url is not a post.
+        setTimeout(() => {
+            if(!_done){
+                window.parent.history.replaceState(null, null, url);
+            }
+        }, 500);
     }
     catch(e){
         console.log("error: ", e);
